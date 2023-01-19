d3c57dc7ab0c4833b9c392827a109dab3be84a09
99p <poccori@gmail.com>
2023-01-19 20:19:02 +0900

    add: MainCameraによるPlayer追尾を追加

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 75f;
+
+    private float vInput;
+    private float hInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,10 @@ public class PlayerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
+        hInput = Input.GetAxis("Horizontal") * rotateSpeed;
         
+        this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
+        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
     }
 }
