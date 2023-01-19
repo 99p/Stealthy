a0517a1371aa7adf45675c02d6348c86cfede280
99p <poccori@gmail.com>
2023-01-19 21:56:29 +0900

    add: Item Capture

---
+++ Assets/Scripts/ItemBehavior.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBehavior : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.name == "Player"){
+            Destroy(this.transform.parent.gameObject);
+            print("<color=yellow>Item collected!</color>");
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -9,11 +9,13 @@ public class PlayerBehavior : MonoBehaviour
 
     private float vInput;
     private float hInput;
+    
+    private Rigidbody _rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -22,7 +24,15 @@ public class PlayerBehavior : MonoBehaviour
         vInput = Input.GetAxis("Vertical") * moveSpeed;
         hInput = Input.GetAxis("Horizontal") * rotateSpeed;
         
-        this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
-        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+        // this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
+        // this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+    }
+    
+    private void FixedUpdate() {
+        Vector3 rotation = Vector3.up * hInput;
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+        
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
     }
 }
