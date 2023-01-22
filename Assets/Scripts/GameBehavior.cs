210c58da0a55cbb05ea5db7822dd3560647084e7
99p <poccori@gmail.com>
2023-01-22 19:13:53 +0900

    add: GUI

--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class GameBehavior : MonoBehaviour
 {
+    public string labelText = "4つのアイテムを集めて自由を勝ち取ろう！";
+    public int maxItems = 4;
+
     private int _itemsCollected = 0;
     public int Items {
         get { return _itemsCollected; }
         set {
             _itemsCollected = value;
-            Debug.LogFormat("Items: {0}", _itemsCollected);
+            
+            if(_itemsCollected >= maxItems){
+                labelText = "アイテムを全部見つけんた！";
+            } else {
+                labelText = $"あと{maxItems - _itemsCollected}つ！！！！！";
+            }
+            
         }
     }
     
@@ -20,5 +29,11 @@ public class GameBehavior : MonoBehaviour
             _playerHP = value;
             Debug.LogFormat("Lives: {0}", _playerHP);
         }
+    }
+    
+    private void OnGUI() {
+        GUI.Box(new Rect(20, 20, 150, 25), $"Player's HP: {_playerHP}");
+        GUI.Box(new Rect(20, 50 ,150, 25), $"集めたアイテム: {_itemsCollected}");
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
     }
 }
