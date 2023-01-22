b639e84580d8c2c19e8f7cc960c7d596256fae85
99p <poccori@gmail.com>
2023-01-22 17:48:39 +0900

    add: GameBehavior

---
+++ Assets/Scripts/GameBehavior.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBehavior : MonoBehaviour
+{
+    private int _itemsDollected = 0;
+    private int _playerHP = 10;
+}
