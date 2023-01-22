298a4b9e5de60e2b1d7092d56cfcd637b972a37f
99p <poccori@gmail.com>
2023-01-22 19:42:03 +0900

    finish: Act8

--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameBehavior : MonoBehaviour
 {
     public string labelText = "4つのアイテムを集めて自由を勝ち取ろう！";
     public int maxItems = 4;
+    
+    public bool shoWinScreen = false;
 
     private int _itemsCollected = 0;
     public int Items {
@@ -15,6 +18,8 @@ public class GameBehavior : MonoBehaviour
             
             if(_itemsCollected >= maxItems){
                 labelText = "アイテムを全部見つけんた！";
+                shoWinScreen = true;
+                Time.timeScale = 0f;
             } else {
                 labelText = $"あと{maxItems - _itemsCollected}つ！！！！！";
             }
@@ -35,5 +40,11 @@ public class GameBehavior : MonoBehaviour
         GUI.Box(new Rect(20, 20, 150, 25), $"Player's HP: {_playerHP}");
         GUI.Box(new Rect(20, 50 ,150, 25), $"集めたアイテム: {_itemsCollected}");
         GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
+        if(shoWinScreen){
+            if(GUI.Button(new Rect(Screen.width/2 - 100, Screen.height/2 - 50, 200, 100), "You WIN!!")){
+                SceneManager.LoadScene(0);
+                Time.timeScale = 1.0f;
+            };
+        }
     }
 }
